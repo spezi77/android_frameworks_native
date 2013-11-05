174bf2fac6cfedea1c78d530c0c66eb1b4eb84b5
Steve Kondik <shade@chemlab.org>
2013-11-05 03:45:47 -0800

    Underp dexroot on cache
    Change-Id: I21e97bfd7888e50c5dadf9fabc859ef9b566cf15
    
    Conflicts:
    	cmds/installd/Android.mk

--- cmds/installd/Android.mk
+++ cmds/installd/Android.mk
@@ -6,8 +6,11 @@ common_src_files := \
 #
 # Static library used in testing and executable
 #
-
 include $(CLEAR_VARS)
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    LOCAL_CFLAGS += -DALLOW_DEXROOT_ON_CACHE
+endif
 
 LOCAL_SRC_FILES := \
     $(common_src_files)
@@ -23,6 +26,10 @@ include $(BUILD_STATIC_LIBRARY)
 #
 
 include $(CLEAR_VARS)
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+    LOCAL_CFLAGS += -DALLOW_DEXROOT_ON_CACHE
+endif
 
 LOCAL_SRC_FILES := \
     installd.c \
