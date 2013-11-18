81b201b501a25bf76fb1f602ae38668ce3ce81ac
winner00 <toverturf@gmail.com>
2013-11-18 16:19:11 -0700

    Revert "Underp dexroot on cache"
    This reverts commit 174bf2fac6cfedea1c78d530c0c66eb1b4eb84b5.
    
    Change-Id: Ibda20d66d733cf331b97bd2610c62a03fc156b64

--- cmds/installd/Android.mk
+++ cmds/installd/Android.mk
@@ -6,11 +6,8 @@ common_src_files := \
 #
 # Static library used in testing and executable
 #
-include $(CLEAR_VARS)
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
-    LOCAL_CFLAGS += -DALLOW_DEXROOT_ON_CACHE
-endif
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
     $(common_src_files)
@@ -26,10 +23,6 @@ include $(BUILD_STATIC_LIBRARY)
 #
 
 include $(CLEAR_VARS)
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-    LOCAL_CFLAGS += -DALLOW_DEXROOT_ON_CACHE
-endif
 
 LOCAL_SRC_FILES := \
     installd.c \
