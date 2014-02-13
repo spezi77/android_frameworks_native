4b6eab5eec296e42dc6044d18d105f31887ed82f
Jeykumar Sankaran <jsanka@codeaurora.org>
2014-02-12 16:08:19 -0800

    frameworks/native: Propagate dirty region in hwc_layer
    This change propagates dirty rect value of each layer down to HWC
    in hwc_layer structure.
    
    Change-Id: I91d87facf4a321b92539de5353faf09b553d4919

--- libs/gui/GLConsumer.cpp
+++ libs/gui/GLConsumer.cpp
@@ -159,6 +159,9 @@ GLConsumer::GLConsumer(const sp<IGraphicBufferConsumer>& bq, uint32_t tex,
 #endif
 
     mConsumer->setConsumerUsageBits(DEFAULT_USAGE_FLAGS);
+#ifdef QCOM_BSP
+    mCurrentDirtyRect.clear();
+#endif
 }
 
 status_t GLConsumer::setDefaultMaxBufferCount(int bufferCount) {
@@ -530,6 +533,9 @@ status_t GLConsumer::updateAndReleaseLocked(const BufferQueue::BufferItem& item)
     mCurrentTimestamp = item.mTimestamp;
     mCurrentFence = item.mFence;
     mCurrentFrameNumber = item.mFrameNumber;
+#ifdef QCOM_BSP
+    mCurrentDirtyRect = item.mDirtyRect;
+#endif
 
     computeCurrentTransformMatrixLocked();
 
@@ -1132,6 +1138,13 @@ status_t GLConsumer::doGLFenceWaitLocked() const {
 
     return NO_ERROR;
 }
+
+#ifdef QCOM_BSP
+Rect GLConsumer::getCurrentDirtyRect() const {
+     Mutex::Autolock lock(mMutex);
+     return mCurrentDirtyRect;
+}
+#endif
 
 void GLConsumer::freeBufferLocked(int slotIndex) {
     ST_LOGV("freeBufferLocked: slotIndex=%d", slotIndex);
